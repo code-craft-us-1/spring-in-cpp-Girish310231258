40e4802370a27eabbd11b96bf5c05711fd8289c8
Girish310231258 <girish.kumar.p@philips.com>
2024-08-30 16:31:20 +0530

    Stats Exercise

--- stats-test.cpp
+++ stats-test.cpp
@@ -3,19 +3,19 @@
 #include "gtest/gtest.h"
 #include <cmath>
 
-TEST(Statistics, ReportsAverageMinMax) {
-    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
-    float epsilon = 0.001;
-    EXPECT_LT(std::abs(computedStats.average - 4.525), epsilon);
-    EXPECT_LT(std::abs(computedStats.max - 8.9), epsilon);
-    EXPECT_LT(std::abs(computedStats.min - 1.5), epsilon);
+TEST(Statistics, ReportsAverageMinMax)
+{
+	auto computedStats = Statistics::ComputeStatistics({ 1.5, 8.9, 3.2, 4.5 });
+	float epsilon = 0.001;
+	EXPECT_LT(std::abs(computedStats.average - 4.525), epsilon);
+	EXPECT_LT(std::abs(computedStats.max - 8.9), epsilon);
+	EXPECT_LT(std::abs(computedStats.min - 1.5), epsilon);
 }
 
-TEST(Statistics, AverageNaNForEmpty) {
-    auto computedStats = Statistics::ComputeStatistics({});
-    //All fields of computedStats (average, max, min) must be
-    //NAN (not-a-number), as defined in math.h
-    
-    //Design the gtest EXPECT statement here.
-    //Use http://www.cplusplus.com/reference/cmath/isnan/
+TEST(Statistics, AverageNaNForEmpty)
+{
+	auto computedStats = Statistics::ComputeStatistics({ NAN, 1.5, 8.9, 4.5 });
+	EXPECT_TRUE(std::isnan(computedStats.average));
+	EXPECT_TRUE(std::isnan(computedStats.min));
+	EXPECT_TRUE(std::isnan(computedStats.max));
 }
--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,24 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+/// <summary>
+/// Compute statistics for a bunch of numbers
+/// </summary>
+Stats Statistics::ComputeStatistics(const std::vector<float>& data)
+{
+	float sum = 0.0;
+	Stats tempObj;
+	tempObj.min = tempObj.max = sum = data[0];
+
+	for (int i = 1; i < data.size(); i++)
+	{
+		if (data[i] < tempObj.min)
+			tempObj.min = data[i];
+		if (data[i] > tempObj.max)
+			tempObj.max = data[i];
+
+		sum = sum + data[i];
+	}
+
+	tempObj.average = sum / data.size();
+	return tempObj;
 }
